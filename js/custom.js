d94ed7c86f13f39dba22f0a4ca84d5c0fc4cc68d
KYSKYS9746 <148739304+KYSKYS9746@users.noreply.github.com>
2024-03-04 18:12:02 +0900

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -1,11 +1,17 @@
 $(function () {
     $('.Main').fullpage({
-        anchors: ['01', '02', '03', '04', '05'],
-        // navigation: true,
+        anchors: ['01', '02', '03', '04', '05', '06'],
+        navigation: true,
         onLeave: function (index, nextIndex, direction) {
 
-            $('.section .footer').removeClass('on');
-            $('.section .footer').eq(nextIndex - 1).addClass('on');
+            $('.section').removeClass('on');
+            $('.section').eq(nextIndex - 1).addClass('on');
+
+            if (direction == 'down') {
+                document.querySelector('#header').classList.add('on')
+            } else {
+                document.querySelector('#header').classList.remove('on')
+            }
         },
     });
 })
